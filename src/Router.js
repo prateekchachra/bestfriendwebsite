4a6f63f48b8d09e9908a8d33c5ff4958dab9cc67
Prateek Chachra <prateekchachra@live.com>
2020-05-11 23:01:52 +0530

    Completed animation

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,8 @@ import React, { Component } from 'react';
 import { renderToStaticMarkup } from "react-dom/server";
 import App from './App';
 import Quiz from './listpages/Quiz';
+import icon from './assets/img/heart.js'
+import ParticlesBg from "particles-bg";
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Header from './components/main/Header';
 import Footer from './components/main/Footer';
@@ -45,10 +47,25 @@ class RouterComponent extends React.Component {
     render()
   
   {
-
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-50, 50],
+      alpha: [0.6, 0],
+      scale: [.1, 0.9],
+      body: icon,
+      position: "all",
+      //color: ["random", "#ff0000"],
+      cross: "dead",
+      random: 10
+    };
     return(
     <Router>
       <Header />
+      <ParticlesBg type="custom" config={config} bg={true} />
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/:language/quiz" component={Quiz} />
